e58aa696652e5c4a82564f106425b00096fdb4a1
Rafal Konstanty <raf.konstanty@gmail.com>
2022-02-01 19:01:12 +0100

    Added ElevatorSystem tests

--- src/pl/konstanty/ElevatorSystemTest.java
+++ src/pl/konstanty/ElevatorSystemTest.java
@@ -1,19 +1,19 @@
 package pl.konstanty;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 class ElevatorSystemTest {
     public static final int SECOND_FLOOR = 2;
-    public static final int BASEMENT_FLOOR = -1;
     public static final int SIXTH_FLOOR = 6;
-    public static final int ELEVATOR_ID = 1;
+    public static final int ELEVATOR_ID_ONE = 1;
+    public static final int ELEVATOR_ID_TWO = 2;
 
     private ElevatorSystem elevatorSystem;
     private List<Elevator> elevators;
@@ -31,12 +31,62 @@ class ElevatorSystemTest {
     @Test
     void goToFloorWhenOnlyPickupOccursTest() {
         //when
-        elevators.get(ELEVATOR_ID).pickup(SIXTH_FLOOR);
-        //then
+        elevators.get(ELEVATOR_ID_ONE).pickup(SIXTH_FLOOR);
         for (int i = 0; i < SIXTH_FLOOR; i++) {
             elevatorSystem.step();
         }
-        assertEquals(SIXTH_FLOOR, elevators.get(ELEVATOR_ID).getCurrentFloor());
+        //then
+        assertEquals(SIXTH_FLOOR, elevators.get(ELEVATOR_ID_ONE).getCurrentFloor());
+    }
+
+    @Test
+    void pickupHandlerOnMultipleElevatorsTest() {
+        //when
+        elevators.get(ELEVATOR_ID_ONE).pickup(SECOND_FLOOR);
+        elevators.get(ELEVATOR_ID_TWO).pickup(SIXTH_FLOOR);
+        for (int i = 0; i < SIXTH_FLOOR; i++) {
+            elevatorSystem.step();
+        }
+        //then
+        assertEquals(SECOND_FLOOR, elevators.get(ELEVATOR_ID_ONE).getCurrentFloor());
+        assertEquals(SIXTH_FLOOR, elevators.get(ELEVATOR_ID_TWO).getCurrentFloor());
+    }
+
+    @Test
+    void checkIfPickedWhenMovingToDestinationFromGroundFloorTest() {
+        //when
+        String message = "";
+        elevators.get(ELEVATOR_ID_ONE).setDestinationFloor(SIXTH_FLOOR);
+        elevators.get(ELEVATOR_ID_ONE).pickup(SECOND_FLOOR);
+        for (int i = 0; i < SIXTH_FLOOR; i++) {
+            elevatorSystem.step();
+        }
+        //then
+        assertNull(elevators.get(ELEVATOR_ID_ONE).getPickupFloors().poll());
+    }
+
+    @Test
+    void checkIfDestinationIsReachedTest() {
+        //when
+        elevators.get(ELEVATOR_ID_ONE).setDestinationFloor(SIXTH_FLOOR);
+        for (int i = 0; i < SIXTH_FLOOR; i++) {
+            elevatorSystem.step();
+        }
+        //then
+        assertEquals(SIXTH_FLOOR, elevators.get(ELEVATOR_ID_ONE).getCurrentFloor());
+    }
+
+    @Test
+    void checkIfMultipleElevatorsDestinationIsReachedTest(){
+        //when
+        elevators.get(ELEVATOR_ID_ONE).setDestinationFloor(SECOND_FLOOR);
+        elevators.get(ELEVATOR_ID_TWO).setDestinationFloor(SIXTH_FLOOR);
+        for (int i = 0; i < SIXTH_FLOOR; i++) {
+            elevatorSystem.step();
+        }
+        //then
+        assertEquals(SECOND_FLOOR, elevators.get(ELEVATOR_ID_ONE).getCurrentFloor());
+        assertEquals(SIXTH_FLOOR, elevators.get(ELEVATOR_ID_TWO).getCurrentFloor());
     }
 
 
